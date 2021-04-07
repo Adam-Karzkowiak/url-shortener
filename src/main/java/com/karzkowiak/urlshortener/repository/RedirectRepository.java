81f73ccaf1dd565ebdafc2f91bcac66421b512d5
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-04-07 16:35:20 +0200

    added methods to find by alias/check if exists by alias

--- src/main/java/com/karzkowiak/urlshortener/repository/RedirectRepository.java
+++ src/main/java/com/karzkowiak/urlshortener/repository/RedirectRepository.java
@@ -4,6 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.karzkowiak.urlshortener.model.Redirect;
 
+import java.util.Optional;
+
 @Repository
 interface RedirectRepository extends JpaRepository<Redirect, Long> {
+    Optional<Redirect> findByAlias(String alias);
+
+    Boolean existsByAlias(String alias);
 }
